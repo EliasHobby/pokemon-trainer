01f3c62a95d7d6ef90285dab494e03be7c04b852
Elias <elias.kloverod.brynestad@no.experis.com>
2022-09-22 15:03:05 +0200

    feat: added models, pages and login form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginformComponent } from './components/loginform/loginform.component';
+import { PokemonCollectionListComponent } from './components/pokemon-collection-list/pokemon-collection-list.component';
+import { PokemonCatalogueComponent } from './components/pokemon-catalogue/pokemon-catalogue.component';
+import { PokemonCollectionComponent } from './components/pokemon-collection/pokemon-collection.component';
+import { PokemonCollectionItemComponent } from './components/pokemon-collection/pokemon-collection-item/pokemon-collection-item.component';
+import { PokemonCatalogueItemComponent } from './components/pokemon-catalogue/pokemon-catalogue-item/pokemon-catalogue-item.component';
+import { PokemonCatalogueItemDetailsComponent } from './components/pokemon-catalogue/pokemon-catalogue-item/pokemon-catalogue-item-details/pokemon-catalogue-item-details.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TrainerPageComponent } from './pages/trainer-page/trainer-page.component';
+import { CataloguePageComponent } from './pages/catalogue-page/catalogue-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginformComponent,
+    PokemonCollectionListComponent,
+    PokemonCatalogueComponent,
+    PokemonCollectionComponent,
+    PokemonCollectionItemComponent,
+    PokemonCatalogueItemComponent,
+    PokemonCatalogueItemDetailsComponent,
+    LandingPageComponent,
+    TrainerPageComponent,
+    CataloguePageComponent
   ],
   imports: [
     BrowserModule,
