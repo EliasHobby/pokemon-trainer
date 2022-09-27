7c4599ae62a24f6e6181aecfc84f993ef2006e19
Elias <elias.kloverod.brynestad@no.experis.com>
2022-09-27 15:24:08 +0200

    merge: merged with main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card'; 
 import { PokemonService } from './services/pokemon-service/pokemon.service';
 import { TrainerService } from './services/trainer.service';
 
