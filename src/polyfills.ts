5d3d71a5c7d6ccc15ce8e1b79a010f228756048d
ffauskanger <42451187+ffauskanger@users.noreply.github.com>
2022-09-27 22:19:28 +0200

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -52,7 +52,6 @@ import 'zone.js';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 
-(window as any).process = {
-  env: { DEBUG: undefined },
-};
-
+    (window as any).global = window;
+    global.Buffer = global.Buffer || require('buffer').Buffer;
+    global.process = require('process');
